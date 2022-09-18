7a92f0ac2eb62d447076d5ebe7b452de95c54332
Asror-Xolmatov <xolmatov3d@gmail.com>
2022-09-18 09:46:23 -0700

    YANGI
    11

---
+++ 9_work.py
@@ -0,0 +1,97 @@
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# cars.sort()
+# print(cars)
+
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# cars.sort(reverse=True)
+# print(cars)
+
+# mehmonlar = ['Odil', 'Hamid', 'Temur', 'Avazbek', 'Farruh', 'Shamsiddin']
+# print(sorted(mehmonlar, reverse=True))
+# print(mehmonlar)
+
+# ages = [12, 98, 34, 65, 34, 76, 11]
+# ages.sort()
+# print(ages)
+# print(sorted(ages, reverse=True))
+
+# fruits = ['pear','banana','apple','watermelon','lemon']
+# fruits.reverse()
+# print(fruits)
+# print(len(fruits))
+
+# sonlar = list(range(0,10))
+# print(sonlar)
+ 
+# juft_sonlar = list(range(0,20,2)) # 0 dan 20 gacha 2 qadam bilan
+# toq_sonlar = list(range(1,20,2))
+# print(juft_sonlar)
+# print(toq_sonlar)
+
+# yillar = list(range(20))
+# print(yillar)
+
+# narhlar = [12000, 22500, 23456, 9800, 5600, 9934, 32874]
+# arzon = min(narhlar)
+# qimat = max(narhlar)
+# jami = sum(narhlar) 
+# print("Eng arzon tavar", arzon , " eng qimat tavar" , qimat,"\njami summa esa:" , jami)
+
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# my_cars = cars [:3]
+# my_cars = cars [2:5]
+# print(my_cars)
+
+# sonlar = [1, 2, 3, 4, 5] 
+# sonlar2 = sonlar
+# sonlar2.append(7)
+# sonlar.append(9)
+# print(sonlar)
+# print(sonlar2)
+
+# sonlar = [1, 2, 3, 4, 5] 
+# sonlar2 = sonlar [:]
+# sonlar2.append(15)
+# sonlar2.append(80)
+# print(sonlar)
+# print(sonlar2)
+
+# O'zgarmas qo'yhat TUPLES
+
+# toys = ('bus','car','bear','dino','snake','lizard')
+# toys = list(toys)
+# toys.append('qilich')
+# toys.append('barbi')
+# print(toys)
+# toys = tuple(toys)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
